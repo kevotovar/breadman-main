746ecb9b0e44e50889e047271d5cae298e3050e1
Kevin <kevintovar@esimez.mx>
2019-08-19 20:02:57 -0500

    Add base fonts and settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,41 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Tailwind`,
-    description: `Gatsby starter styled with Tailwind`,
-    author: `@taylorbryant`
+    title: `Breadman`,
+    description: `Agencia`,
+    author: `@taylorbryant`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-tailwind`,
-        short_name: `starter`,
+        name: `Breadman`,
+        short_name: `Breadman`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#4dc0b5`,
         display: `minimal-ui`,
-        icon: `src/images/tailwind-icon.png`
-      }
+        icon: `src/images/tailwind-icon.png`,
+      },
     },
     `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-plugin-purgecss`,
       options: {
         tailwind: true,
-        purgeOnly: [`src/css/style.css`]
-      }
+        purgeOnly: [`src/css/style.css`],
+      },
     },
-    `gatsby-plugin-offline`
-  ]
-};
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Open+Sans:300,400,600,700,800`,
+          `Raleway:300,400,500,600,700,900`,
+        ],
+        display: 'swap',
+      },
+    },
+  ],
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 // See https://tailwindcss.com/docs/configuration for details
 
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        'breadman-grey': '#BFBFBF',
+        'breadman-black': '#262027',
+        'breadman-red': '#F05472',
+      },
+      fontFamily: {
+        display: ['"Raleway"', 'sans-serif'],
+        body: ['"Open Sans"', 'sans-serif'],
+        sans: ['"Open Sans"', 'sans-serif'],
+        serif: ['"Raleway"', 'sans-serif'],
+      },
+    },
+  },
   variants: {},
-  plugins: []
-};
+  plugins: [],
+}
