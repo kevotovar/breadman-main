c3cc23adf74667b19f669f1b0ed8a6ce1edd5276
Kevin Tovar <kevintovar@esimez.mx>
2019-08-20 23:39:59 -0500

    Add identidad de marca section

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,6 +57,32 @@ function IndexPage() {
           </div>
         </div>
       </div>
+      <div className="md:h-screen p-8 flex items-center bg-breadman-red">
+        <div className="md:flex mx-auto max-w-6xl items-center justify-between">
+          <div className="w-full md:w-1/5">
+            <h5 className="text-breadman-black-light font-serif font-black text-4xl mb-4 uppercase leading-none">
+              Identidad <br /> <span className="text-white">de Marca</span>
+            </h5>
+            <p>
+              Son todos los aspectos visuales tangibles que forman parte de la
+              marca cómo: logotípo, línea gráfica, tipografía, colores,
+              anuncios, papelería, etc.
+              <br />
+              <br />
+              En pocas palabras es: la manera visual de decirle a tus clientes
+              quién y eres y cómo te ves.
+            </p>
+          </div>
+          <div className="w-full mt-6 md:mt-0 md:w-3/5">
+            <p className="text-breadman-black-light text-6xl leading-none font-serif font-black mb-6">
+              Los estilos vienen y van. El buen diseño es un{' '}
+              <span className="text-white">lenguaje</span>, nunca un estilo.
+            </p>
+            <div className="bg-white h-2 w-24 mb-4"></div>
+            <p className="font-light text-xl">Massimo Vignelli</p>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
