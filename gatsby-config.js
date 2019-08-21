8a50a60d8d4c5e881631aa352dc175937d3cf4e9
Kevin Tovar <kevintovar@esimez.mx>
2019-08-21 11:34:51 -0500

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,5 +40,21 @@ module.exports = {
     },
     'gatsby-plugin-root-import',
     'gatsby-plugin-sitemap',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-146160716-1',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+      },
+    },
   ],
 }
