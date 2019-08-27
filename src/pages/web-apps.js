65bfa59dad8c78b973035806a874369bec4b47aa
Kevin Tovar <kevintovar@esimez.mx>
2019-08-27 09:10:59 -0500

    Add web apps

--- src/pages/web-apps.js
+++ src/pages/web-apps.js
@@ -2,25 +2,29 @@ import React from 'react'
 import Layout from 'src/components/layout'
 import ContactFooter from 'src/components/ContactFooter'
 
-export default function Identity() {
+export default function WebApps() {
   return (
     <Layout>
       <div className="min-h-screen bg-breadman-black pt-24 pb-12 md:pt-64 px-6">
         <div className="flex flex-wrap items-center justify-between lg:justify-around mt-24 max-w-6xl mx-auto">
           <div className="flex-1">
             <h1 className="text-white text-5xl md:text-6xl uppercase font-serif font-black leading-none">
-              IDENTIDAD
+              WEB | <span className="text-breadman-red">APPS</span>
             </h1>
             <h3 className="leading-none text-breadman-red mb-4">
-              LOGO / DISEÑO GRÁFICO / DIRECCIÓN DE ARTE
+              SITIO WEB / DESARROLLO / APPS
             </h3>
             <p className="text-white max-w-sm">
-              Creamos una identidad gráfica sólida, que transmita la
-              personalidad y los valores de tu empresa.
+              Usamos métodos de optimización de recursos para que nuestros
+              sitios funcionen a alta velocidad en conexiones 3g móviles,
+              asegurándonos de siempre calificar en google con una puntuación de
+              90+ mejorando el posicionamiento en búsquedas.
               <br />
               <br />
-              Nos encargamos de todo lo que tenga que ver con la imagen de tu
-              empresa, desde el logotipo, hasta el diseño gráfico del sitio web.
+              En el ámbito del desarrollo móvil usamos React Native, siendo el
+              mejor ambiente de desarrollo para aplicaciones Android y iOS, con
+              esto nos aseguramos de mantener la consistencia entre plataformas
+              para asegurar la mejor experiencia de usuario.
             </p>
           </div>
           <div className="w-full md mt-16 md:mt-0 md:w-1/2 lg:w-1/3 md:pl-12">
@@ -33,28 +37,26 @@ export default function Identity() {
               </div>
               <div>
                 <p className="text-white text-sm text-center border-b-2 border-breadman-red px-4 pb-4 mb-4">
-                  Logotipo
+                  Sitio web
                 </p>
                 <p className="text-white text-sm text-center border-b-2 border-breadman-red px-4 pb-4 mb-4">
-                  Línea gráfica
+                  eCommerce
                 </p>
                 <p className="text-white text-sm text-center border-b-2 border-breadman-red px-4 pb-4 mb-4">
-                  Diseño gráfico
+                  Landing page
                 </p>
                 <p className="text-white text-sm text-center border-b-2 border-breadman-red px-4 pb-4 mb-4">
-                  Diseño web
+                  Desarrollo web
                 </p>
-                <p className="text-white text-sm text-center pb-4 mb-4">
-                  Dirección de arte
-                </p>
+                <p className="text-white text-sm text-center pb-4 mb-4">Apps</p>
               </div>
             </div>
           </div>
         </div>
         <ContactFooter
-          title="¿Tienes el logo que quieres?"
+          title="¿Sabes si tu negocio necesita un sitio web?"
           description={`
-          Si no estas contento con el logotipo que tienes actualmente, platícanos que no te gusta de él, y que cambios le harías, o bien, si aún no tienes logo también podrías contarnos que traes en mente, en fin, estamos para ayudarte.
+          Hacer un sitio web no es cosa menor y por eso es normal que te surjan muchas dudas sobre si es o no necesario, las ventajas que ofrece, los beneficios y el crecimiento que representa tener presencia online. Escríbenos y recibe asesoría gratuita sobre todo lo que necesitas saber de sitios web para tu negocio.
         `}
         />
       </div>
